3e38da90b148b816dee83377653ca9736858dbf0
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-24 23:14:36 -0300

    <Create> Footer Component

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+    return (
+      <div className="bg-gray-800 text-white p-10 text-center mt-auto">
+        <h1 className="text-lg mb-4">Feito por Gabriel Lima De Souza</h1>
+        <a
+          href="https://github.com/GabrielLima2803/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="no-underline text-white"
+        >
+          <h3 className="text-sm italic cursor-pointer pb-2 mb-0">Código no Github</h3>
+        </a>
+      </div>
+    );
+  }
+  
