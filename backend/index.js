34d8b678f1b1bb0330f99d81f0196c3944e60716
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-24 23:12:51 -0300

    <Create> Boot file

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require('cors');
+const { connectToMongoDb } = require("./src/config/connect");
+const app = express();
+const URL = require("./src/models/url");
+
+
+const urlRouter = require("./src/routes/urlRouter");
+const PORT = 3000;
+
+connectToMongoDb("mongodb://127.0.0.1:27017/EncurtadorUrl").then(
+  console.log("Mongodb connected")
+);
+app.use(cors());
+app.use(express.json());
+
+app.use("/url", urlRouter);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor Rodando na porta: ${PORT}`);
+});
